f9f35dbea6406fa58a1b7767f8589c644e73a29e
Gabriel Ribeiro <gabriel.dev@h4money.com.br>
2024-01-25 21:35:37 -0300

    refactor to better handle the response

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,22 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let sucess_request = request_line == "GET / HTTP/1.1";
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = String::from("HTTP/1.1 200 OK");
+    let (status_line, path) = if sucess_request {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    handle_response(stream, status_line, path)
+}
 
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write((&response).as_bytes()).unwrap();
-        return;
-    }
-
-    let status_line = "HTTP/1.1 404 NOT FOUND";
-    let contents = fs::read_to_string("404.html").unwrap();
+fn handle_response(mut stream: TcpStream, status_line: &str, path: &str) {
+    let contents = fs::read_to_string(path).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write(response.as_bytes()).unwrap();
 }
